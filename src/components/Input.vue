e12ccf694c223adce8f42a7a1fe9d05af6fc6aa6
Rens Oliemans <rens.oliemans@gmail.com>
2020-04-06 18:22:19 +0200

    start on project

---
+++ src/components/Input.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="input">
+        <label for="text-input">Enter some text</label> <br />
+        <input type="text" id="text-input">
+
+        <b-button variant="outline-primary">Convert to binary</b-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Input"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="main">
+        <Input />
+    </div>
+</template>
+
+<script>
+import Input from "@/components/Input";
+
+export default {
+    name: "Main",
+    components: {
+        Input
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
